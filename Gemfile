56afd64a2128bf8b3afe787ed2aae75082f188fb
grosser <grosser.michael@gmail.com>
2012-06-08 11:51:16 -0700

    bla

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :rubygems
 gemspec
 
-gem 'activesupport', '3.2.5'
-gem 'test-unit'#, :path => "../test-unit"
+gem 'activesupport', '2.3.14' #'3.2.5'
+gem 'test-unit', '1.1.1', :path => "../test-unit"
+gem 'test-unit-activesupport'
--- test/soft_deletion_test.rb
+++ test/soft_deletion_test.rb
@@ -1,17 +1,7 @@
 require 'test/unit'
 
 require 'test/unit/ui/console/testrunner'
-
-Test::Unit::UI::Console::TestRunner.class_eval do
-  alias_method :attach_to_mediator_without_fix, :attach_to_mediator
-
-  def attach_to_mediator
-    attach_to_mediator_without_fix
-    @mediator.add_listener(Test::Unit::TestCase::STARTED, &method(:test_started))
-    @mediator.add_listener(Test::Unit::TestCase::FINISHED, &method(:test_finished))
-  end
-end
-
+require 'test/unit/active_support'
 require 'active_support/test_case'
 
 class SoftDeletionTest < ActiveSupport::TestCase
