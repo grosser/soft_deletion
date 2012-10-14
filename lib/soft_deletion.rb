c69b59f6fc468efb05d70258380d02c65c201d2d
grosser <grosser.michael@gmail.com>
2012-10-14 16:50:03 -0700

    make 1.8 safe and make it possible to not have it auto-included

--- lib/soft_deletion.rb
+++ lib/soft_deletion.rb
@@ -3,3 +3,5 @@ require 'soft_deletion/version'
 require 'soft_deletion/core'
 require 'soft_deletion/dependency'
 require 'soft_deletion/setup'
+
+ActiveRecord::Base.send(:include, SoftDeletion::Setup)
