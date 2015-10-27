291dc89d7cfbfa0bdb1de0398f7cbe2c6aa83fc2
Robert Tatsumi <rtatsumi@zendesk.com>
2015-10-27 09:22:40 -0700

    memoize deleted_at

--- lib/soft_deletion/core.rb
+++ lib/soft_deletion/core.rb
@@ -72,7 +72,7 @@ module SoftDeletion
     end
 
     def mark_as_deleted
-      self.deleted_at = Time.now unless self.deleted?
+      self.deleted_at ||= Time.now
     end
 
     def mark_as_undeleted
